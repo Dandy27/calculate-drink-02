88ce062fbd5181f5c4aaa6233bb7cd76fe32ffcb
dandy <danielsbarbosa27@gmail.com>
2022-09-21 00:10:09 -0300

    Construindo Repository e Service d eusuário

--- calculate_drink_02/lib/app/app_module.dart
+++ calculate_drink_02/lib/app/app_module.dart
@@ -3,6 +3,10 @@ import 'package:provider/provider.dart';
 
 import 'app_widget.dart';
 import 'core/database/sqlite_connection_factory.dart';
+import 'repositories/user/user_repository.dart';
+import 'repositories/user/user_repository_impl.dart';
+import 'services/user_service.dart';
+import 'services/user_service_impl.dart';
 
 class AppModule extends StatelessWidget {
   const AppModule({Key? key}) : super(key: key);
@@ -10,9 +14,22 @@ class AppModule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [Provider(create: (_) => SqliteConnectionFactory(),
-      lazy: false,
-      )],
+      providers: [
+        Provider(
+          create: (_) => SqliteConnectionFactory(),
+          lazy: false,
+        ),
+        Provider<UserRepository>(
+          create: (context) => UserRepositoryImpl(
+            firebaseAuth: context.read(),
+          ),
+        ),
+        Provider<UserService>(
+          create: (context) => UserServiceImpl(
+            userRepository: context.read(),
+          ),
+        )
+      ],
       child: const AppWidget(),
     );
   }
